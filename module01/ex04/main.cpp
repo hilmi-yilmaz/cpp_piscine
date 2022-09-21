a98b560e3573548885cfe808fc3a3386834f4ccf
Hilmi Yilmaz <hilmiyilmaz.1998@gmail.com>
2022-09-21 14:21:39 +0200

    module01 done

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -5,6 +5,7 @@
 int	main(int argc, char *argv[]) {
 
 	if (argc != 4) {
+		std::cout << "No inputs provided." << std::endl;
 		return 1;
 	}
 
@@ -24,32 +25,24 @@ int	main(int argc, char *argv[]) {
 
 	std::string line;
 	while (std::getline(input_file, line)) {
-		// size_t index = line.find(argv[2]);
-		// if (index == std::string::npos) {
-
-		// 	output_file << line << std::endl;
-		// 	continue;
-		// }
-		// output_file << line.substr(0, index) << argv[3] << line.substr(index + strlen(argv[2])) << std::endl;
 		size_t	i = 0;
 		while (true) {
-			std::cout << "i = " << i << " | line = " << line.substr(i) << std::endl;
 			size_t index = line.find(argv[2], i);
-			std::cout << "index: " << index << std::endl;
 			if (index == std::string::npos) {
-				
 				output_file << line.substr(i);
 				break;
 			} else {
 
-				std::cout << "else: " << line.substr(index + strlen(argv[2])) << std::endl;
+				// Before the found string
+				output_file << line.substr(i, index - i);
 
-				output_file << line.substr(0, index) << argv[3];
-				i += index + strlen(argv[2]);
+				// Change s1 to s2
+				output_file << argv[3];
+				
+				i = index + strlen(argv[2]);
 			}
 		}
 		output_file << std::endl;
-	
 	}
 	input_file.close();
 	output_file.close();
